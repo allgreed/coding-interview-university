7bbd67f31201f55a73e88ec27aa856b9fdb6df51
allgreed <olixem@gmail.com>
2017-11-07 22:08:25 +0100

    Added a stub for hash_table

--- course/data_structures/hash_tables/cpp/hash_table.cpp
+++ course/data_structures/hash_tables/cpp/hash_table.cpp
@@ -1,5 +1,74 @@
-// #include <stdexcept>
-// import what's needed for the implementation
+#include <stdexcept>
 
-// pragma region
-    // implementation
+#pragma region Privates
+
+#pragma endregion
+
+#pragma region Constructors, destructor, assignment operators
+
+template <typename T>
+HashTable<T>::HashTable()
+{
+
+}
+
+template <typename T>
+HashTable<T>::~HashTable()
+{
+
+}
+
+template <typename T>
+HashTable<T>::HashTable(const HashTable<T>& rhs)
+{
+
+}
+
+template <typename T>
+HashTable<T>& HashTable<T>::operator=(const HashTable<T>& rhs)
+{
+
+}
+
+template <typename T>
+HashTable<T>::HashTable(HashTable<T>&& rhs)
+{
+
+}
+
+template <typename T>
+HashTable<T>& HashTable<T>::operator=(HashTable<T>&& rhs)
+{
+
+}
+
+#pragma endregion
+
+#pragma region Public functions
+
+template <typename T>
+void HashTable<T>::add(std::string key, T value)
+{
+
+}
+
+template <typename T>
+bool HashTable<T>::exists(std::string key)
+{
+
+}
+
+template <typename T>
+T HashTable<T>::get(std::string key)
+{
+
+}
+
+template <typename T>
+void HashTable<T>::remove(std::string key)
+{
+
+}
+
+
+#pragma endregion
--- course/data_structures/hash_tables/cpp/hash_table.hpp
+++ course/data_structures/hash_tables/cpp/hash_table.hpp
@@ -1,11 +1,57 @@
 #pragma once
 // Warning: this library may throw
 
-// import other types
+#include <string>
 
 // constants
+constexpr size_t DEFAULT_SIZE = 10;
 
-// classes
+// typedef struct HashTable_locator
+// {
+//     bool is_valid;
+//     size_t to_index;
+// } HashTable_locator;
 
-// Only if class is using generic templates
+// typedef enum
+// {
+//     HASHTABLE_ELEM_EMPTY,
+//     HASHTABLE_ELEM_DELETED,
+//     HASHTABLE_ELEM_OCCUPIED
+// } HASHTABLE_ELEM_STATE;
+
+// struct _element
+// {
+//     T value;
+//     std::string key;
+//     // HASHTABLE_ELEM_STATE state;
+// };
+
+template <typename T>
+class HashTable
+{
+    protected:
+
+
+    protected:
+        // HashTable_element* data;
+        size_t _capacity;
+        size_t _size;
+
+    protected:
+
+    public:
+        HashTable();
+        ~HashTable();
+        HashTable(const HashTable<T>& rhs);
+        HashTable<T>& operator=(const HashTable<T>& rhs);
+        HashTable(HashTable<T>&& rhs);
+        HashTable<T>& operator=(HashTable<T>&& rhs);
+
+    public:
+        void add(std::string key, T value);
+        bool exists(std::string key);
+        T get(std::string key);
+        void remove(std::string key);
+};
+
 #include "hash_table.cpp"
